49ea7523014a529e67907f8329f3292a434dba59
mindheist <dragonzsnake@gmail.com>
2016-06-01 21:06:21 -0700

    Basic Graph Implementation

--- implementing_graph.py
+++ implementing_graph.py
@@ -24,3 +24,31 @@ class Graph:
     def __init__(self):
         self.numVertices = 0
         self.VertList = {}
+
+    def addVertex(self,key):
+        newVertex = vertex(key)
+        self.numVertices +=1
+        self.VertList[key] = newVertex
+        return newVertex
+
+    def addEdge(self,f,t,weight):
+        if f not in self.VertList:
+            newVertex = VertList(f)
+        if t not in self.VertList:
+            newVertex = VertList(t)
+        self.VertList[f].addNeighbour(self.VertList[t],weight)
+
+    def getVertex(self,key):
+        return self.VertList[key]
+
+    def getVertices(self):
+        return self.VertList.keys()
+
+
+my_graph = Graph()
+my_graph.addVertex("A")
+print my_graph.getVertices()
+my_graph.addVertex("B")
+print my_graph.getVertices()
+my_graph.addEdge("A","B",10)
+print my_graph.getVertex("A").getConnections()
